c2bc32f17362dbf414ffb36c6bbed91ba6e62e72
EthanStasse <ethan.stasse88@gmail.com>
2026-02-24 11:49:52 +0100

    modif

--- API/serre_API/serre/urls.py
+++ API/serre_API/serre/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mysite project.
+URL configuration for serre project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -14,12 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
+    path("API/", include("API.urls")),
     path("admin/", admin.site.urls),
 ]
-
--- README.md
+++ README.md
@@ -5,6 +5,9 @@ python -m venv C:\Users\ethan\Desktop\Stage\serre\API\venv
 PS C:\Users\ethan\Desktop\Stage\serre\API> venv\Scripts\activate
 pip install pyserial
 py -m pip install Django==6.0.2
+django-admin startproject serre serre_API
+dans serre_API : 
+python manage.py runserver 0.0.0.0:8000
 
 usr:pi
 mdp:pi
