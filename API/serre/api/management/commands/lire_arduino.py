05bbefd7f565b7b8f3e2823e37709ce50c309a7f
EthanStasse <ethan.stasse88@gmail.com>
2026-02-25 15:03:37 +0100

    Revert "test servo btn"
    This reverts commit 2fef0a3b60362e973978fb5493baa435c8465c26.

--- API/serre/api/management/commands/lire_arduino.py
+++ API/serre/api/management/commands/lire_arduino.py
@@ -25,7 +25,7 @@ class Command(BaseCommand):
                         sol=data['sol'],
                         temp=data['temp'],
                         hum=data['hum'],
-                        lumiere=data['lumiere'],
+                        lumière=data['lumiere'],
                         periode=data['periode'],
                         servo=data['servo'],
                         pompe=data['pompe'],
