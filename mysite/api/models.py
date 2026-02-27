af446e97093fb78fc4e5d531d8c495b494bd7eb3
EthanStasse <ethan.stasse88@gmail.com>
2026-02-27 17:31:14 +0100

    model

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class blogPost(models.model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_add_now=True) 
+
+    def __str__(self):
+        return self.title
