994a1d7187c3dd8cbcae07316b663a2bfb789369
EthanStasse <ethan.stasse88@gmail.com>
2026-03-02 15:40:17 +0100

    clean up

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -56,13 +56,6 @@ def index(request):
 
 
 @api_view(['GET'])
-def get_serre(request):
-    serre = Serre.objects.all().order_by('-created_at')[:10]
-    serializer = SerreSerializer(serre, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
 def last_serre(request):
     lastserre = Serre.objects.latest('created_at')
     serializer = SerreSerializer(lastserre)
