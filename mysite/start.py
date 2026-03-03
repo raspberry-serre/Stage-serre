d29c7b96b2e037a05707c51369c0053af62a1de9
raspberry <serre.connecter12@gmail.com>
2026-03-03 17:45:14 +0100

    add auto migration to start.py

--- mysite/start.py
+++ mysite/start.py
@@ -1,6 +1,10 @@
 import subprocess
 import sys
 
+migrations = subprocess.Popen([sys.executable, "manage.py", "makemigrations"])
+migrations.wait()
+migrate = subprocess.Popen([sys.executable, "manage.py", "migrate"])
+migrate.wait()
 arduino = subprocess.Popen([sys.executable, "manage.py", "com_arduino"])
 django = subprocess.Popen([sys.executable, "manage.py", "runserver", "0.0.0.0:8000"])
 
