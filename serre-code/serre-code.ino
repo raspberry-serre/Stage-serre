ce50da755d8eb89a5eed17864e4100b8699a6cdd
EthanStasse <ethan.stasse88@gmail.com>
2026-03-17 14:21:57 +0100

    send pump on

--- serre-code/serre-code.ino
+++ serre-code/serre-code.ino
@@ -228,6 +228,10 @@ void loop() {
     Serial.print("\",");
     Serial.print("\"debit\":\"");
     Serial.print(pump_debit);
+    Serial.print("\",");
+    Serial.print("\"pump_on_time\":\"");
+    Serial.print((PUMP_ON_TIME / 1000));
+    Serial.print("\",");
     Serial.println("}");
   }
 
