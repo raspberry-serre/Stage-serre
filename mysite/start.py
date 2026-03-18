fbb75b83930e70a91e52d0704d30f4a7f7cc45c3
raspberry <serre.connecter12@gmail.com>
2026-03-18 11:12:18 +0100

    take pictures

--- mysite/start.py
+++ mysite/start.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 
+subprocess.run([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
 migrations = subprocess.Popen([sys.executable, "manage.py", "makemigrations"])
 migrations.wait()
 migrate = subprocess.Popen([sys.executable, "manage.py", "migrate"])
@@ -8,6 +9,8 @@ migrate.wait()
 
 arduino = subprocess.Popen([sys.executable, "manage.py", "com_arduino"])
 django = subprocess.Popen([sys.executable, "manage.py", "runserver", "0.0.0.0:8000"])
+picture = subprocess.Popen([sys.executable, "manage.py", "picture"])
 
 django.wait()
 arduino.terminate()
+picture.terminate()
