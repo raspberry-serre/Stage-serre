0308588dc23ce124010311267873716c4c5206ab
EthanStasse <ethan.stasse88@gmail.com>
2026-04-07 15:03:38 +0200

    add comments

--- mysite/start.py
+++ mysite/start.py
@@ -1,13 +1,17 @@
 import subprocess
 import sys
 
+#install/upgrade pip
 subprocess.run([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
+#instale les dépendances
 subprocess.run([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+#applique les migrations
 migrations = subprocess.Popen([sys.executable, "manage.py", "makemigrations"])
 migrations.wait()
 migrate = subprocess.Popen([sys.executable, "manage.py", "migrate"])
 migrate.wait()
 
+#lance les processus pour le serveur Django, la communication avec l'Arduino et la prise de photos
 arduino = subprocess.Popen([sys.executable, "manage.py", "com_arduino"])
 django = subprocess.Popen([sys.executable, "manage.py", "runserver", "0.0.0.0:8000"])
 picture = subprocess.Popen([sys.executable, "manage.py", "picture"])
